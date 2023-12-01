c04eef4c6a15366c2f863cc6e251da082a119bfc
大内千夏 <chinatsu.ouchi@ysinc.co.jp>
2023-12-01 19:23:42 +0900

    api追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
-  return <div className='App'>AWS Sample App</div>
-}
+export const App = () => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const API_INVOKE_URL = 'https://p6av0qjgsh.execute-api.ap-northeast-1.amazonaws.com/dev'
 
-export default App
+  const onSubmit = async () => {
+    if (!firstName || !lastName) return
+    await fetch(API_INVOKE_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ firstName, lastName }),
+    })
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  return (
+    <div className='App'>
+      <form>
+        <label>First Name :</label>
+        <input type='text' id='fName' onChange={(event) => setFirstName(event.target.value)} />
+        <label>Last Name :</label>
+        <input type='text' id='lName' onChange={(event) => setLastName(event.target.value)} />
+        <button type='button' onClick={onSubmit}>
+          Call API
+        </button>
+      </form>
+    </div>
+  )
+}
